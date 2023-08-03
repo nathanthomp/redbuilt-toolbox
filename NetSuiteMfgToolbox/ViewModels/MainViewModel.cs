2b2e29175dd9a5a091aa3c07bb21ac8630343f46
Nathan Thompson <nthompson@redbuilt.com>
2023-08-03 15:06:51 -0400

    UI implementation using MVVM

---
+++ NetSuiteMfgToolbox/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetSuiteMfgToolbox.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set 
+			{ 
+				_currentView = value;
+				OnPropertyChanged();
+			}
+		}
+
+        private UnreleaseViewModel UnreleaseViewModel { get; set; }
+        /*
+		 * To add new views, add the above line with the new view model type.
+		 */
+
+        public MainViewModel()
+		{
+			this.UnreleaseViewModel = new UnreleaseViewModel();
+            /*
+			 * To add new views, add the above line with the new view model type.
+			 */
+            CurrentView = UnreleaseViewModel;
+		}
+
+
+	}
+}
---
+++ NetSuiteMfgToolbox/ViewModels/UnreleaseViewModel.cs
@@ -0,0 +1,40 @@
+﻿using NetSuiteMfgToolbox.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NetSuiteMfgToolbox.ViewModels
+{
+    public class UnreleaseViewModel : ObservableObject
+    {
+		private UnreleaseModel Model { get; set; }
+
+		private string _soNumber;
+
+		public string SONumber
+		{
+			get { return _soNumber; }
+			set 
+			{
+				_soNumber = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public ICommand UnreleaseCommand { get; set; }
+
+		public UnreleaseViewModel()
+		{
+			this.Model = new UnreleaseModel();
+			this.UnreleaseCommand = new RelayCommand(o => 
+			{
+				this.Model.Unrelease(SONumber);
+			});
+        }
+	}
+}
