a233eaa728f49ce0c1937c2066528d69e3ee0178
Vezzoso, Brent <bvezzoso@RedBuilt.com>
2023-08-08 11:38:26 -0600

    stub out for unrelease

--- NetSuiteMfgToolbox/Models/UnreleaseModel.cs
+++ NetSuiteMfgToolbox/Models/UnreleaseModel.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows;
+using RedBuilt.NetSuite;
 
 namespace NetSuiteMfgToolbox.Models
 {
@@ -7,8 +9,19 @@ namespace NetSuiteMfgToolbox.Models
     {
         public async Task Unrelease(string soNumber)
         {
-            await Task.Delay(1000);
-            MessageBox.Show($"Unreleased {soNumber}");
+            //await Task.Delay(1000);
+            //MessageBox.Show($"Unreleased {soNumber}");
+            RBSalesOrder salesOrder = RBSalesOrder.GetSalesOrder(soNumber);
+            List<RBWorkOrder> allWorkOrders = RBWorkOrder.GetRelatedWorkOrdersByCreatedFrom(salesOrder.InternalId);
+            foreach(RBWorkOrder order in allWorkOrders)
+            {
+                unReleaseWorkOrder(order);
+            }
+        }
+        private void unReleaseWorkOrder(RBWorkOrder workOrder) 
+        { 
+            //if this is a parent work order, we need to change status from Released to Plan and clear WO Initialize checkbox
+            
         }
     }
 }
