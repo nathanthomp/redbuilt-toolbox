baebdb62b3f74a434f93f9be58d8f3bdcbee8df4
Nathan Thompson <nthompson@redbuilt.com>
2024-03-08 15:25:59 -0500

    Create command heirarchy and implement unrelease command

---
+++ src/NetSuiteMfgToolbox/Commands/UnreleaseCommand.cs
@@ -0,0 +1,28 @@
+﻿using NetSuiteMfgToolbox.Models;
+
+namespace NetSuiteMfgToolbox.Commands
+{
+    internal class UnreleaseCommand : CommandBase
+    {
+        private readonly UnreleaseModel _model;
+        private readonly string _soNumber;
+
+        public UnreleaseCommand(UnreleaseModel model, string soNumber)
+        {
+            _model = model;
+            _soNumber = soNumber;
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            // TODO:
+            // SO Number must pass RegEx
+            return base.CanExecute(parameter);
+        }
+
+        public override void Execute(object parameter)
+        {
+            _model.Unrelease(_soNumber);
+        }
+    }
+}
