3453c8d2e008a5a82706f0681fb4ee2f14eda88e
Nathan Thompson <nthompson@redbuilt.com>
2024-03-13 08:45:08 -0400

    Initial refactoring

--- src/NetSuiteMfgToolbox/ViewModels/MainViewModel.cs
+++ src/NetSuiteMfgToolbox/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using RedBuilt.NetSuite;
+﻿using NetSuiteMfgToolbox.Commands;
+using RedBuilt.NetSuite;
+using System.Collections.Generic;
 using System.Windows.Input;
 
 namespace NetSuiteMfgToolbox.ViewModels
@@ -17,50 +19,57 @@ namespace NetSuiteMfgToolbox.ViewModels
 			}
 		}
 
-        private UnreleaseViewModel UnreleaseViewModel { get; set; }
-        private UpdateBOMRevisionViewModel UpdateBOMRevisionViewModel { get; set; }
+        private NSClient.NSEnvironment _environment = NSClient.NSEnvironment.Production;
 
-		public ICommand UnreleaseViewCommmand { get; set; }
-		public ICommand UpdateBOMRevisionCommand { get; set; }
-
-        public MainViewModel()
-		{
-			this.UnreleaseViewModel = new UnreleaseViewModel();
-            this.UpdateBOMRevisionViewModel = new UpdateBOMRevisionViewModel();
-
-			this.UnreleaseViewCommmand = new RelayCommand(o =>
-			{
-				this.CurrentView = this.UnreleaseViewModel;
-			});
-
-			this.UpdateBOMRevisionCommand = new RelayCommand(o =>
-			{
-				this.CurrentView = this.UpdateBOMRevisionViewModel;
-			});
-
-            this.CurrentView = UnreleaseViewModel;
-		}
-
-        public string azureAppId = "31fe67d2-9f85-4860-8abe-00e96abd3de6"; // Azure AD > App registrations > RB Mfg Toolbox
-        public string azureAppSecret = "~KO8Q~.rJe8nNP-3lUSRuQGVDVh4JLGZd_u2Pbkt"; // PWVault1 > RBMfgToolbox
-
-        private NSClient.NSEnvironment _environment = NSClient.NSEnvironment.Sandbox;
         public NSClient.NSEnvironment Environment
         {
             get { return _environment; }
             set { _environment = value; }
         }
 
-        private NSClient _nsClient;
-        public NSClient nsClient
-        {
-            get { return _nsClient; }
-            set 
-            { 
-                _nsClient = value;
-                this.UnreleaseViewModel.nsClient = _nsClient;
-                this.UpdateBOMRevisionViewModel.nsClient = _nsClient;
-            }
-        }
+
+
+        private UnreleaseViewModel _unreleaseViewModel;
+        private UpdateBOMRevisionViewModel _updateBOMRevisionViewModel;
+
+		public ICommand UnreleaseViewCommmand { get; }
+		public ICommand UpdateBOMRevisionCommand { get; }
+
+        public MainViewModel()
+		{
+            _unreleaseViewModel = new UnreleaseViewModel();
+            _updateBOMRevisionViewModel = new UpdateBOMRevisionViewModel();
+
+            UnreleaseViewCommmand = new NavigateCommand(); // this, UnreleaseViewModel
+            UpdateBOMRevisionCommand = new NavigateCommand(); // this, UpdateBOMRevisionViewModel
+
+            CurrentView = _unreleaseViewModel;
+		}
+
+        // ----------------------------------------- CAN BE REMOVED -----------------------------------------
+
+        //public string azureAppId = "31fe67d2-9f85-4860-8abe-00e96abd3de6"; // Azure AD > App registrations > RB Mfg Toolbox
+        //public string azureAppSecret = "~KO8Q~.rJe8nNP-3lUSRuQGVDVh4JLGZd_u2Pbkt"; // PWVault1 > RBMfgToolbox
+
+        //private NSClient.NSEnvironment _environment = NSClient.NSEnvironment.Sandbox;
+        //public NSClient.NSEnvironment Environment
+        //{
+        //    get { return _environment; }
+        //    set { _environment = value; }
+        //}
+
+        //private NSClient _nsClient;
+        //public NSClient nsClient
+        //{
+        //    get { return _nsClient; }
+        //    set 
+        //    { 
+        //        _nsClient = value;
+        //        this.UnreleaseViewModel.nsClient = _nsClient;
+        //        this.UpdateBOMRevisionViewModel.nsClient = _nsClient;
+        //    }
+        //}
+
+        // ----------------------------------------- CAN BE REMOVED -----------------------------------------
     }
 }
